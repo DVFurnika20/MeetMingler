0fdfc587d63362ab978a95b1af2ebfc5998c1cf8
Alexander Manov <aemanov20@codingburgas.bg>
2024-12-04 21:37:55 +0200

    Document EventFilter.cs

--- MeetMingler/MeetMingler.BLL/Filters/EventFilter.cs
+++ MeetMingler/MeetMingler.BLL/Filters/EventFilter.cs
@@ -2,9 +2,18 @@ namespace MeetMingler.BLL.Filters;
 
 public class EventFilter
 {
+    /// <summary>
+    /// Metadata key/value pairs to include in the result set
+    /// </summary>
     public List<string> IncludeMetadataKeys { get; set; } = [];
     
+    /// <summary>
+    /// Metadata filters to apply to the result set
+    /// </summary>
     public List<Dictionary<string, string>> MetadataFilters { get; set; } = [];
 
+    /// <summary>
+    /// General text search to filter out both titles and descriptions
+    /// </summary>
     public string TextSearch { get; set; } = null!;
 }
