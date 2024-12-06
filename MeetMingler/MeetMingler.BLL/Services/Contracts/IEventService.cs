7def830736fd3200762a099aa1325799a9f82335
DVFurnika20 <DVFurnika20@codingburgas.bg>
2024-12-06 14:55:08 +0200

    Update Event service

--- MeetMingler/MeetMingler.BLL/Services/Contracts/IEventService.cs
+++ MeetMingler/MeetMingler.BLL/Services/Contracts/IEventService.cs
@@ -1,9 +1,9 @@
 using MeetMingler.BLL.Filters;
 using MeetMingler.BLL.Models.Event;
 using MeetMingler.BLL.Services.Contracts.Generic;
-
+ 
 namespace MeetMingler.BLL.Services.Contracts;
-
+ 
 public interface IEventService : IGenericCreateService<EventVM, EventIM>,
     IGenericFilteredCollectionService<EventVM, EventFilter>
 {
@@ -11,24 +11,27 @@ public interface IEventService : IGenericCreateService<EventVM, EventIM>,
     /// Retrieve an event by its unique identifier.
     /// </summary>
     /// <param name="id">The event's unique identifier</param>
+    /// <param name="cf">Cancellation token</param>
     /// <returns>The view model of an event</returns>
-    Task<EventVM> GetEventByIdAsync(Guid id);
-    
+    Task<EventVM> GetEventByIdAsync(Guid id, CancellationToken cf = default);
+ 
     /// <summary>
     /// Retrieve all events created by a user
     /// </summary>
     /// <param name="creatorId">The user's unique identifier</param>
     /// <param name="includeMetadataKeys">The metadata key/value pairs to include in the resultset</param>
+    /// <param name="cf">Cancellation token</param>
     /// <returns>A collection of events</returns>
-    Task<IQueryable<EventVM>> GetEventsByCreatorIdAsync(Guid creatorId, List<string> includeMetadataKeys);
-    
+    Task<IQueryable<EventVM>> GetEventsByCreatorIdAsync(Guid creatorId, List<string> includeMetadataKeys, CancellationToken cf = default);
+ 
     /// <summary>
     /// Retrieve all distinct metadata values for a given metadata key
     /// </summary>
     /// <param name="metadataKey">Metadata key</param>
+    /// <param name="cf">Cancellation token</param>
     /// <returns>A collection of values related to the metadata key</returns>
-    Task<IQueryable<string>> GetDistinctMetadataValuesAsync(string metadataKey);
-    
+    Task<IQueryable<string>> GetDistinctMetadataValuesAsync(string metadataKey, CancellationToken cf = default);
+ 
     // TODO: add methods for updating single event
     // TODO: add methods for cancelling/reinstating event
     // TODO: add methods for updating adding/updating/deleting event metadata
--- MeetMingler/MeetMingler.BLL/Services/Implementations/EventService.cs
+++ MeetMingler/MeetMingler.BLL/Services/Implementations/EventService.cs
@@ -3,84 +3,76 @@ using MeetMingler.BLL.Filters;
 using MeetMingler.BLL.Models;
 using MeetMingler.BLL.Models.Event;
 using MeetMingler.BLL.Services.Contracts;
+using MeetMingler.DAL.Data;
 using MeetMingler.DAL.Models;
 using Microsoft.EntityFrameworkCore;
-
+ 
 namespace MeetMingler.BLL.Services.Implementations;
-
-public class EventService : IEventService
+ 
+public class EventService(IMapper mapper, ApplicationDbContext context) : IEventService
 {
-    private readonly IMapper _mapper;
-    private readonly DbContext _context;
-
-    public EventService(IMapper mapper, DbContext context)
-    {
-        _mapper = mapper;
-        _context = context;
-    }
-
     public async Task<EventVM?> CreateAsync(EventIM im, CancellationToken cf = default)
     {
-        var eventEntity = _mapper.Map<Event>(im);
-        _context.Set<Event>().Add(eventEntity);
-        await _context.SaveChangesAsync(cf);
-        return _mapper.Map<EventVM>(eventEntity);
+        var eventEntity = mapper.Map<Event>(im);
+        context.Set<Event>().Add(eventEntity);
+        await context.SaveChangesAsync(cf);
+        return mapper.Map<EventVM>(eventEntity);
     }
-
-    public async Task<EventVM> GetEventByIdAsync(Guid id)
+ 
+    public async Task<EventVM> GetEventByIdAsync(Guid id, CancellationToken cf = default)
     {
-        var eventEntity = await _context.Set<Event>().FindAsync(id);
-        return _mapper.Map<EventVM>(eventEntity);
+        var eventEntity = await context.Set<Event>().AsNoTracking().FirstOrDefaultAsync(e => e.Id == id, cf);
+        return mapper.Map<EventVM>(eventEntity);
     }
-
-    public async Task<IQueryable<EventVM>> GetEventsByCreatorIdAsync(Guid creatorId, List<string> includeMetadataKeys)
+ 
+    public async Task<IQueryable<EventVM>> GetEventsByCreatorIdAsync(Guid creatorId, List<string> includeMetadataKeys, CancellationToken cf = default)
     {
-        var query = _context.Set<Event>().Where(e => e.CreatorId == creatorId).AsQueryable();
-
+        var query = context.Set<Event>().AsNoTracking().Where(e => e.CreatorId == creatorId).AsQueryable();
+ 
         query = includeMetadataKeys.Aggregate(query, (current, key) => current.Include(key));
-
-        var eventEntities = await query.ToListAsync();
-        return eventEntities.AsQueryable().Select(e => _mapper.Map<EventVM>(e));
+ 
+        var eventEntities = await query.ToListAsync(cf);
+        return eventEntities.AsQueryable().Select(e => mapper.Map<EventVM>(e));
     }
-
-    public async Task<IQueryable<string>> GetDistinctMetadataValuesAsync(string metadataKey)
+ 
+    public async Task<IQueryable<string>> GetDistinctMetadataValuesAsync(string metadataKey, CancellationToken cf = default)
     {
-        var values = await _context.Set<MeetMingler.DAL.Models.EventMetadata>()
+        var values = await context.Set<MeetMingler.BLL.Models.Event.EventMetadata>()
             .Where(em => em.Key == metadataKey)
             .Select(em => em.Value)
             .Distinct()
-            .ToListAsync();
-
+            .ToListAsync(cf);
+ 
         return values.AsQueryable();
     }
-
+ 
     public async Task<IQueryable<EventVM>> GetFilteredCollectionAsync(EventFilter filter, CancellationToken cf = default)
     {
-        var query = _context.Set<Event>().AsQueryable();
-
+        var query = context.Set<Event>().AsQueryable();
+ 
         if (!string.IsNullOrEmpty(filter.TextSearch))
         {
             query = query.Where(e => e.Title.Contains(filter.TextSearch) || e.Description.Contains(filter.TextSearch));
         }
-
+ 
         var eventEntities = await query.ToListAsync(cf);
-        return eventEntities.AsQueryable().Select(e => _mapper.Map<EventVM>(e));
+        return eventEntities.AsQueryable().Select(e => mapper.Map<EventVM>(e));
     }
-
+ 
     public async Task<BaseCollectionVM<EventVM>> GetAllPaginatedAndFilteredAsync(PaginationOptions paginationOptions, EventFilter filter, CancellationToken cf = default)
     {
-        var query = _context.Set<Event>().AsQueryable();
-
+        var query = context.Set<Event>().AsQueryable();
+ 
         if (!string.IsNullOrEmpty(filter.TextSearch))
         {
             query = query.Where(e => e.Title.Contains(filter.TextSearch) || e.Description.Contains(filter.TextSearch));
         }
-        
+ 
         var items = query
             .Skip((paginationOptions.Page - 1) * paginationOptions.PageSize)
             .Take(paginationOptions.PageSize)
-            .Select(e => _mapper.Map<EventVM>(e));
-
+            .Select(e => mapper.Map<EventVM>(e));
+ 
         return new BaseCollectionVM<EventVM>
         {
             Items = items,
