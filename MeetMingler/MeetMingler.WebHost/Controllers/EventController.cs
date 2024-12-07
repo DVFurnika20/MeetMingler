8a0ca16c2cfa1d57968151e2b41bc058ad033c98
DVFurnika20 <DVFurnika20@codingburgas.bg>
2024-12-07 20:51:39 +0200

    Add cancellation tokens as parameters for event service methods in event controller

--- MeetMingler/MeetMingler.WebHost/Controllers/EventController.cs
+++ MeetMingler/MeetMingler.WebHost/Controllers/EventController.cs
@@ -13,9 +13,9 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
     : BaseController(authService, currentUser)
 {
     [HttpPost]
-    public async Task<ActionResult<EventVM>> Create([FromBody] EventIM eventIm)
+    public async Task<ActionResult<EventVM>> Create([FromBody] EventIM eventIm, CancellationToken ct)
     {
-        var result = await eventService.CreateAsync(eventIm);
+        var result = await eventService.CreateAsync(eventIm, ct);
         if (result == null)
         {
             return BadRequest();
@@ -25,9 +25,9 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
     }
 
     [HttpGet("{id:guid}")]
-    public async Task<ActionResult<EventVM>> GetById([FromRoute] Guid id)
+    public async Task<ActionResult<EventVM>> GetById([FromRoute] Guid id, CancellationToken ct)
     {
-        var result = await eventService.GetByIdAsync(id);
+        var result = await eventService.GetByIdAsync(id, ct);
         if (result == null)
         {
             return NotFound();
@@ -38,31 +38,32 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
 
     [HttpGet("{creatorId:guid}")]
     public async Task<ActionResult<IEnumerable<EventVM>>> GetByCreator([FromRoute] Guid creatorId,
-        [FromQuery] List<string> includeMetadataKeys, [FromQuery] PaginationOptions paginationOptions)
+        [FromQuery] List<string> includeMetadataKeys, [FromQuery] PaginationOptions paginationOptions,
+        CancellationToken ct)
     {
-        var result = await eventService.GetCollectionByCreatorAsync(creatorId, includeMetadataKeys, paginationOptions);
+        var result = await eventService.GetCollectionByCreatorAsync(creatorId, includeMetadataKeys, paginationOptions, ct);
         return Ok(result);
     }
 
     [HttpGet]
     public async Task<ActionResult<BaseCollectionVM<EventVM>>> GetAllPaginatedAndFiltered(
-        [FromQuery] PaginationOptions paginationOptions, [FromQuery] EventFilter filter)
+        [FromQuery] PaginationOptions paginationOptions, [FromQuery] EventFilter filter, CancellationToken ct)
     {
-        var result = await eventService.GetAllPaginatedAndFilteredAsync(paginationOptions, filter);
+        var result = await eventService.GetAllPaginatedAndFilteredAsync(paginationOptions, filter, ct);
         return Ok(result);
     }
 
     [HttpGet("{metadataKey}")]
-    public async Task<ActionResult<IEnumerable<string>>> GetDistinctMetadataValues([FromRoute] string metadataKey)
+    public async Task<ActionResult<IEnumerable<string>>> GetDistinctMetadataValues([FromRoute] string metadataKey, CancellationToken ct)
     {
-        var result = await eventService.GetDistinctMetadataValuesAsync(metadataKey).ToListAsync();
+        var result = await eventService.GetDistinctMetadataValuesAsync(metadataKey).ToListAsync(ct);
         return Ok(result);
     }
 
     [HttpPatch("{id:guid}")]
-    public async Task<ActionResult> SetCancelled([FromRoute] Guid id, [FromBody] bool cancelled)
+    public async Task<ActionResult> SetCancelled([FromRoute] Guid id, [FromBody] bool cancelled, CancellationToken ct)
     {
-        var result = await eventService.SetCancelledAsync(id, cancelled);
+        var result = await eventService.SetCancelledAsync(id, cancelled, ct);
         if (!result)
         {
             return Forbid();
@@ -72,9 +73,9 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
     }
 
     [HttpPost("{eventId:guid}")]
-    public async Task<ActionResult<EventVM>> AddMetadata([FromRoute] Guid eventId, [FromBody] EventMetadataIM metadata)
+    public async Task<ActionResult<EventVM>> AddMetadata([FromRoute] Guid eventId, [FromBody] EventMetadataIM metadata, CancellationToken ct)
     {
-        var result = await eventService.AddMetadataAsync(eventId, metadata);
+        var result = await eventService.AddMetadataAsync(eventId, metadata, ct);
         if (result == null)
         {
             return Forbid();
@@ -84,9 +85,9 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
     }
 
     [HttpDelete("{eventId:guid}/{key}")]
-    public async Task<ActionResult> DeleteMetadata([FromRoute] Guid eventId, [FromRoute] string key)
+    public async Task<ActionResult> DeleteMetadata([FromRoute] Guid eventId, [FromRoute] string key, CancellationToken ct)
     {
-        var result = await eventService.DeleteMetadataAsync(eventId, key);
+        var result = await eventService.DeleteMetadataAsync(eventId, key, ct);
         if (result == null)
         {
             return Forbid();
@@ -96,9 +97,9 @@ public class EventController(IAuthService authService, ICurrentUser currentUser,
     }
 
     [HttpDelete("{id:guid}")]
-    public async Task<ActionResult> Delete([FromRoute] Guid id)
+    public async Task<ActionResult> Delete([FromRoute] Guid id, CancellationToken ct)
     {
-        var result = await eventService.DeleteAsync(id);
+        var result = await eventService.DeleteAsync(id, ct);
         if (!result)
         {
             return Forbid();
