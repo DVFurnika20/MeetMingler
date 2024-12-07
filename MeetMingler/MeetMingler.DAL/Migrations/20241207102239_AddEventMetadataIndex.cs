c2e24c9171d59da279023c89004bf2a8042d6ef4
Alexander Manov <aemanov20@codingburgas.bg>
2024-12-07 12:23:53 +0200

    Update migrations

---
+++ MeetMingler/MeetMingler.DAL/Migrations/20241207102239_AddEventMetadataIndex.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MeetMingler.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventMetadataIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_EventMetadataEntries_EventId",
+                table: "EventMetadataEntries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "EventMetadataEntries",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventMetadataEntries_EventId_Key",
+                table: "EventMetadataEntries",
+                columns: new[] { "EventId", "Key" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_EventMetadataEntries_EventId_Key",
+                table: "EventMetadataEntries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "EventMetadataEntries",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EventMetadataEntries_EventId",
+                table: "EventMetadataEntries",
+                column: "EventId");
+        }
+    }
+}
